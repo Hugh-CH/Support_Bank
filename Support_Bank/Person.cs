e83c14cc33b36ae07de1fc78b80d97e6cfbfa8e3
Hugh Hudson <Hugh.Hudson@softwire.com>
2021-11-18 13:30:14 +0000

    added calculate balance for person class

--- Support_Bank/Person.cs
+++ Support_Bank/Person.cs
@@ -22,12 +22,25 @@ namespace Support_Bank
             listOfPaymentsIn.Add(transaction);
         }
 
+        public float CalculateBalance()
+        {
+            float balance = 0;
+            foreach (Transaction payment in listOfPaymentsIn)
+            {
+                balance += payment.amount;
+            }
+            foreach (Transaction payment in listOfPaymentsOut)
+            {
+                balance -= payment.amount;
+            }
+            return balance;
+        }
+
         public Person(string name)
         {
             this.name = name;
             listOfPaymentsIn = new List<Transaction>();
             listOfPaymentsOut = new List<Transaction>();
         }
-
     }
 }
